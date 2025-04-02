982a8f08e84f1a8da1b8ffeb6190443fafb28107
ansifpk <pkansif39@gmail.com>
2025-04-02 16:01:31 +0530

    add config

--- frontent/src/service/client.ts
+++ frontent/src/service/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const client = axios.create({
-        baseURL:import.meta.env.VITE_API_URL,
+        baseURL:import.meta.env.BASE_URL,
         headers:{
             "Content-Type": "multipart/form-data"
         }
